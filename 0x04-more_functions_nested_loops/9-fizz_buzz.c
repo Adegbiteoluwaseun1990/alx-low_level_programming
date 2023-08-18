8ee9fa4fd0502635d838a299fabbfaf2da347042
Adegbiteoluwaseun1990 <adegbiteoluwaseun99@gmail.com>
2023-08-17 18:57:20 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,25 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main -Entry point
- *
- * Return: Always 0
+ * main - fizbuzz
+ * Return: 0
  */
 
 int main(void)
 {
-	int x = 1;
+	int i;
 
-	for (; x < 100; x++)
+	for (i = 1; i < 100; i++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
-		       printf("FizzBuzz ");
-		else if (x % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if (x % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
-			printf("%d ", x);
+			printf("%d ", i);
 	}
 	printf("Buzz\n");
-
 	return (0);
 }
